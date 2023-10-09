a8d48b8f9cfe8c0f8c339b274baa3a76c093216f
Jon Ball <91580027+j-a-ball@users.noreply.github.com>
2023-10-08 19:10:33 -0700

    Add files via upload
    update argparse

---
+++ convert_pdfs.py
@@ -0,0 +1,24 @@
+from multiprocessing import Pool
+from tqdm import tqdm
+import argparse
+import os
+
+from pdf2txt import pdf2txt
+
+if __name__ == "__main__":
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--pdf_dir", required=True, help="path to directory containing PDFs", default="pdfs")
+    parser.add_argument("-t", "--txt_dir", help="path to directory to save text files", default="txts")
+    parser.add_argument("-n", "--n_workers", type=int, default=8)
+    args = parser.parse_args()
+    # Load pdf parser
+    pdf2txt = pdf2txt(args.pdf_dir, args.txt_dir)
+    pdf_files = sorted([f for f in os.listdir(args.pdf_dir) if f.endswith(".pdf")])
+    # Convert the PDFs to text in parallel processes
+    with Pool(args.n_workers) as p:
+        txts = list(tqdm(p.imap(pdf2txt.convert, pdf_files), total=len(pdf_files)))
+    #for pdf_file in tqdm(pdf_files):
+    #    pdf2txt.convert(pdf_file)
+    print(f"\nDone! Converted {len(txts)} PDFs to text files.")
